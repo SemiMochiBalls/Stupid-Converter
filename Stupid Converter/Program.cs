cd53f559754bbe6e9417de163601a3ba543835b3
Sachi Reyes <semizerogravity@gmail.com>
2023-02-22 13:20:07 -0500

    added american menu and get year functions

--- Stupid Converter/Program.cs
+++ Stupid Converter/Program.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Stupid_Converter
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            americanMenu();
+        }
+        static int getYear()
+        {
+            return DateTime.Now.Year;
+        }
+
+        public static void europeanMenu()
+        {
+
+        }
+        public static void americanMenu()
+        {   int year = getYear();
+            Console.WriteLine("CHOOSE A MEASRUENT TO CONVERT TO AMERICAN");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("A.) CRAPPY EURO LINE MEASURMENT TO PATRIOTIC");
+            Console.WriteLine("AMERICAN FOOTBALLFIELDS");
+            Console.WriteLine("B.) WIMP DICKED EUROPEAN FASTNESS MEASRUMENT TO");
+            Console.WriteLine("FAST AS FUCK AMERICAN BALD EAGLES");
+            Console.WriteLine("C. SCARY EURO (COMMIE) MEASRUMENT OF BIG SQUARE TO");
+            Console.WriteLine($"AMERICAN FOOTBALL FIELDS RAHHHH LETS GO SUPERBOWL {year}");
         }
     }
 }
